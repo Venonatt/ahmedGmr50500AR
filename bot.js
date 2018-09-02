dc1f9948524d5be75f70d6449e723f285d4b71bb
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-09-02 22:31:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -333,6 +333,19 @@ gg.send({embed : new Discord.RichEmbed()
  }
 })
 
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 
 
 client.login(process.env.BOT_TOKEN);
