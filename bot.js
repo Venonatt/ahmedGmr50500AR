15212b3e665ee72b35564cd14efcf57f0e680919
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-09-02 12:59:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,39 @@ client.user.setGame(`System`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+const bannedwords = [
+  "#help",
+  "#credit",
+  "#profile",
+  "#rep",
+  "#top",
+  "%level",
+  "%تقديم",
+  "-play",
+  "-stop",
+  "-p",
+  "-s",
+  "!invites",
+  "!top"
+
+]
+client.on('message', message => {
+  var Muted = message.guild.roles.find("name", "muted");
+  var warn = message.guild.roles.find("name", "warn");
+  if(bannedwords.some(word => message.content.includes(word))) {
+  if(message.channel.id !== '474606523092566018') return;
+  if (message.author.bot) return;
+  if(message.member.roles.has(warn)) return;
+  if(!message.member.roles.has(warn.id)) {
+  message.member.addRole(warn)
+  message.reply("**`تم اعطائك تحذير لاستخدام اوامر البوت فى الشات العام` 😠**")
+  }
+  if(message.member.roles.has(warn.id)) {
+      message.member.addRole(Muted)
+      message.member.removeRole(warn)
+      message.reply("**`تم اعطائك ميوت كتابى تواصل مع احد اعضاء الادارة لازالتة` 🤐**")
+  }
+  }
+  })
+
 client.login(process.env.BOT_TOKEN);
