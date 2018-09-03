42add2ef78b760959626af6d0b44cc60cb7bfd1f
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-09-03 15:51:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -394,30 +394,5 @@ client.on('message', async message => {
       });
 
 
-const devs = ['451828723017056268' , '' , '' , ''];
-const adminprefix = "$";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
-      message.channel.sendMessage(`**✅   ${argresult}**`)
-  }
-  });
-
-
 
 client.login(process.env.BOT_TOKEN);
