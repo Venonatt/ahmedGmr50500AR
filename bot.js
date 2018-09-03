451b7a0f21e48861426ee994daf50085b9f24b10
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-09-03 21:52:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -461,39 +461,6 @@ gg.send({embed : new Discord.RichEmbed()
 
 
 
-if (message.author.bot) return;
-if (message.content.startsWith(prefix + "contact")) {
-if (!message.channel.guild) return;
-
-
-
-let args = message.content.split(" ").slice(1).join(" ");
-
-
-
-client.users.get("451828723017056268").send(
-    "\n" + "**" + "● السيرفر :" + "**" +
-    "\n" + "**" + "» " + message.guild.name + "**" +
-    "\n" + "**" + " ● المرسل : " + "**" +
-    "\n" + "**" + "» " + message.author.tag + "**" +
-    "\n" + "**" + " ● الرسالة : " + "**" +
-    "\n" + "**" + args + "**")
-
-let embed = new Discord.RichEmbed()
-     .setAuthor(message.author.username, message.author.avatarURL)
-     .setDescription(':mailbox_with_mail: تم ارسال الرسالة الى صاحب البوت بنجاح')
-     .setThumbnail(message.author.avatarURL)
-     .setFooter("FOX Community©")
-                                                
-
-message.channel.send(embed);
-
-
-}
-    
-});
-
-
 
 
 client.login(process.env.BOT_TOKEN);
