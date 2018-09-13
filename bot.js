b5616e75c9099d10ba5edcdc7c86b7443540fa38
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-09-13 17:04:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ Discord.RichEmbed()
 .addField('Message', args)
 .setThumbnail(message.author.avatarURL)
 .setFooter(copy, client.user.avatarURL);
-m.send({ embed: bc })
+m.send({ embed: })
 msg.delete();
 })
 })
