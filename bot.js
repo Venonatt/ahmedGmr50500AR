d9e7390ac3e7882e5748bd4e3ef931ab6e3ee86c
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-09-14 21:34:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,23 +31,29 @@ client.user.setGame(`System`,"http://twitch.tv/S-F")
 
  
 
-var Nedjmo1 = [
-"هلا والله نورت سيرفر"
-]
 
 client.on('message', message => {
     if (message.content === 'هلا') {
-        message.reply(Nedjmo1);
+        message.reply('هلا والله نورت سيرفر');
     }
 });
 
 
 
+client.on("message", message => {
 
-
-client.on('ready', () => {
-client.user.setGame(`System`)
+            if (message.content.startsWith(prefix + "bc2")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
 });
+
   
   
  client.on("guildMemberAdd", member => {
