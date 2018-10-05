bf2942adc7a81ccfef6f397c5545807d1c804b04
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-10-05 18:04:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,6 +49,25 @@ client.on('message', message => {
 }).catch(console.error)
 }) ;
 
+client.on('ready', function(){
+  client.user.setStatus("dnd");
+    var ms = 40000 ;	
+    var setGame = ['$help','Register'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
+}, ms);	
+	
+});
+
 
 client.on('message', message => {
   if(message.content.startsWith(prefix + "تقديم")) {
