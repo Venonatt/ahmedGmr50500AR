8b72da2d3fff19771aa648c33c8a305bc05588c5
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-12-27 16:58:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -485,7 +485,7 @@ var msg;
       message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
       message.channel.sendMessage("", {embed: {
         title: "`` Chat Deleted ``",
-        color: 0x06DF00,
+        color: #ff0004,
         footer: {
 
         }
