f738e1ea219e08eed0409189bc9bab5b58f07246
Venonatt <42704102+Venonatt@users.noreply.github.com>
2019-01-01 13:21:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,7 +52,7 @@ client.on('message', message => {
 client.on('ready', function(){
   client.user.setStatus("dnd");
     var ms = 40000 ;	
-    var setGame = ['$help','Register'];	
+    var setGame = ['*help','Register'];	
     var i = -1;	
     var j = 0;	
     setInterval(function (){	
