31e4e56a5e38232ae5eba242db719adc4b0fed85
Venonatt <42704102+Venonatt@users.noreply.github.com>
2019-01-01 16:12:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -285,70 +285,15 @@ gg.send({embed : new Discord.RichEmbed()
 
 
 client.on('message', message => {
-            if (message.content.startsWith(prefix + prefix + "help" + "-system")) {
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('     ** *تقديم** ' ,' **Rank support** ')
-.addField('     ** *egister** ' ,' **للتسجيل في سيرفر** ')
-.addField('     ** *clear ** ' ,' **لمسح شات** ')
-.addField('     ** *bc1 ** ' ,' **مع امبد للنشر في سيرفر** ')
-.addField('     ** *bc2 ** ' ,' ** للكل بدون امبد للنشر في سيرفر** ')
-.setColor('#ff5e00')
-  message.channel.sendEmbed(embed);
-    }
-});
-
-
-client.on('message', message => {
             if (message.content.startsWith(prefix + "help")) {
      let embed = new Discord.RichEmbed()
 .setThumbnail(message.author.avatarURL)
-.addField('     **$$help-system** ' ,' **System يعرض لك اوامر بوت** ')
-.addField('     **$$help-codes**  ' ,' **يعرض لك اوامر اكواد للتضيفها في سيرفر** ')
-.setColor('#ff5e00')
-  message.channel.sendEmbed(embed);
-    }
-});
-
-client.on('message', message => {
-            if (message.content.startsWith(prefix + prefix + "help" + "-codes")) {
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('     **$$h-codes-js**  ' ,' **js يعرض لك اكواد بلغة ** ')
-.addField('     **$$h-codes-py**  ' ,' **py يعرض لك اكواد بلغة ** ')
-.addField('     **$$h-codes-io**  ' ,' **io يعرض لك اكواد بلغة ** ')
-.addField('     **$$h-codes-html**  ' ,' **html يعرض لك اكواد بلغة ** ')
-.addField('     **$$h-codes-eric**  ' ,' **eric يعرض لك اكواد بلغة ** ')
-.setColor('#ff5e00')
-  message.channel.sendEmbed(embed);
-    }
-});
-
-client.on('message', message => {
-            if (message.content.startsWith(prefix + prefix + "h" + "-codes" +"-js")) {
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('     **سوف يتم اضافة اكواد** ')
-.setColor('#ff5e00')
-  message.channel.sendEmbed(embed);
-    }
-});
-
-client.on('message', message => {
-            if (message.content.startsWith(prefix + prefix + "h" + "-codes" +"-py")) {
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('     **سوف يتم اضافة اكواد** ')
-.setColor('#ff5e00')
-  message.channel.sendEmbed(embed);
-    }
-});
-
-client.on('message', message => {
-            if (message.content.startsWith(prefix + prefix + "h" + "-codes" +"-io")) {
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('     **سوف يتم اضافة اكواد** ')
+.addField('     ** $تقديم** ' ,' **Rank support** ')
+.addField('     ** Register** ' ,' **للتسجيل في سيرفر** ')
+.addField('     ** $clear ** ' ,' **لمسح شات** ')
+.addField('     ** $bc1 ** ' ,' **مع امبد للنشر في سيرفر** ')
+.addField('     ** $bc2 ** ' ,' ** للكل بدون امبد للنشر في سيرفر** ')
+.addField('     ** $new ** ' ,' ** لاضافة بطاقة** ')
 .setColor('#ff5e00')
   message.channel.sendEmbed(embed);
     }
@@ -356,25 +301,95 @@ client.on('message', message => {
 
 
 
-client.on('message', message => {
-            if (message.content.startsWith(prefix + prefix + "h" + "-codes" +"-html")) {
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('     **سوف يتم اضافة اكواد** ')
-.setColor('#ff5e00')
-  message.channel.sendEmbed(embed);
-    }
-});
 
-client.on('message', message => {
-            if (message.content.startsWith(prefix + prefix + "h" + "-codes" +"-eric")) {
-     let embed = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.addField('     **سوف يتم اضافة اكواد** ')
-.setColor('#ff5e00')
-  message.channel.sendEmbed(embed);
+
+
+client.on("message", (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+    if (message.content.toLowerCase().startsWith(prefix + `help-ticket`)) {
+      const embed = new Discord.RichEmbed()
+      .setTitle(`:mailbox_with_mail: Help`)
+      .setColor(0xCF40FA)
+      .setDescription(`مرحباٌ! I'm ${client.user.username}, هذا البوت خاص للتذاكر لمساعره فريق العمل و هذه هي الاوامر:`)
+      .addField(`Tickets`, `[${prefix}new]() > لفتح تذكره جديده و منشنه اعضاء دعم السرفر \n[${prefix}close]() > لغلق التذكره التي تم فتحها من قبل الدعم`)
+      .addField(`Other`, `[${prefix}help]() > لرؤيه قائمه الاوامر \n[${prefix}ping]() > لمعرفه البينق الخاص للبوت \n[${prefix}about]() > لمعرفه كل شي عن البوت`)
+      message.channel.send({ embed: embed });
     }
-});
+  
+    if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
+      message.channel.send(`Hoold on!`).then(m => {
+      m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+      });
+  }
+  
+  if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+      const reason = message.content.split(" ").slice(1).join(" ");
+      if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`هذا السيرفر ليس لديه \`Support Team\` صنع رتبة, لذلك لن يتم فتح التذكرة.\nاذا كنت تمتلك administrator, إنشاء اسم بهذا الاسم بالضبط وإعطائه للمستخدمين الذين يمكنهم مشاهدة التذاكر.`);
+      if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`انت بالفعل لديك تذكره مفتوحه.`);
+      message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+          let role = message.guild.roles.find("name", "Support Team");
+          let role2 = message.guild.roles.find("name", "@everyone");
+          c.overwritePermissions(role, {
+              SEND_MESSAGES: true,
+              READ_MESSAGES: true
+          });
+          c.overwritePermissions(role2, {
+              SEND_MESSAGES: false,
+              READ_MESSAGES: false
+          });
+          c.overwritePermissions(message.author, {
+              SEND_MESSAGES: true,
+              READ_MESSAGES: true
+          });
+          let mrx = new Discord.RichEmbed()
+          .setColor('RANDOM')
+          .setAuthor(message.author.tag,message.author.avatarURL)
+          .setDescription(`:white_check_mark: تم إنشاء تذكرتك, #${c.name}.`)
+          .setTimestamp()
+          message.channel.sendEmbed(mrx);
+          const embed = new Discord.RichEmbed()
+          .setColor(0xCF40FA)
+          .addField(`مرحباً ${message.author.username}!`, `من فضلك وضح لماذا قمت بفتح التذكره مع بعض التفاصيل. مسؤلي السرفر سوف يكونوا موجودين في اسرع وقت للمساعده.`)
+          .setTimestamp();
+          c.send({ embed: embed });
+      }).catch(console.error);
+  }
+  if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+      if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لا يمكنك استخدام أمر الإغلاق خارج قناة التذاكر.`);
+  
+      message.channel.send(`هل أنت متاكد؟ بمجرد تأكيد, لا يمكنك عكس هذا العمل!!\nللتأكيد ، اكتب \`-confirm\`. سوف ينتهي المهلة خلال 10 ثوانٍ ويتم إلغاؤها.`)
+      .then((m) => {
+        message.channel.awaitMessages(response => response.content === '-confirm', {
+          max: 1,
+          time: 10000,
+          errors: ['time'],
+        })
+        .then((collected) => {
+            message.channel.delete();
+          })
+          .catch(() => {
+            m.edit('انتهى إغلاق التذاكر ، لم يتم إغلاق التذكرة.').then(m2 => {
+                m2.delete();
+            }, 3000);
+          });
+      });
+  }
+  
+  });
+  
+  client.on('message', message => {
+      if (message.content.startsWith(prefix + 'about')) {
+      if (message.author.bot) return
+      if (!message.guild) return message.reply('**:x: This Command Only In Server**')
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setTitle(':mailbox_with_mail: about')
+      .setDescription(`I am ${client.user.username}, and I will try my best to help everyone! If I am in a discord server, people can use me to create tickets in order`)
+      .setFooter(`${client.user.username}`)
+      message.author.sendEmbed(embed)
+      }
+  });
 
 
 
